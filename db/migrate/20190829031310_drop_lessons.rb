311064c2747f3446110eb2a040201fd286f781ed
bwalterscoding <bwalters@my.waketech.edu>
2019-08-29 03:15:29 +0000

    removed redundant db table

---
+++ db/migrate/20190829031310_drop_lessons.rb
@@ -0,0 +1,5 @@
+class DropLessons < ActiveRecord::Migration[5.1]
+  def change
+    drop_table :lessons
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20190829020704) do
+ActiveRecord::Schema.define(version: 20190829031310) do
 
   create_table "class_reports", force: :cascade do |t|
     t.datetime "created_at", null: false
@@ -18,11 +18,6 @@ ActiveRecord::Schema.define(version: 20190829020704) do
     t.text "student_name"
     t.decimal "student_level"
     t.text "class_notes"
-  end
-
-  create_table "lessons", force: :cascade do |t|
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
   end
 
   create_table "users", force: :cascade do |t|
